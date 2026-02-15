6807a40212b58cc13ce0011fec39c53b798b2607
southeane <southeane@southeanes-MacBook-Pro.local>
2026-02-15 13:48:35 +0700

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { get_db_config } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    expandVariables: true,
+  }), 
+  TypeOrmModule.forRootAsync({
+    useFactory: async () => {
+      return get_db_config().options;
+    },
+  })
+],
   controllers: [AppController],
   providers: [AppService],
 })
